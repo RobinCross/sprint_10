d2e37d75057177bcc15305d53947ddcd02d9d28b
RobinCross <robincross321@gmail.com>
2022-10-26 13:34:14 -0600

    adds a bigger query and refactors python for connection db into function

--- module_1/sqlite_example.py
+++ module_1/sqlite_example.py
@@ -1,24 +1,23 @@
 import sqlite3
 import queries as q
+import pandas as pd
 
+# DB Connect function
+def connect_to_db(db_name='rpg_db.sqlite3'):
+    return sqlite3.connect(db_name)
 
-# step 0 - Import sqlite3
-import sqlite3
-
-# step 1
-# Connect to the database
-connection = sqlite3.connect('rpg_db.sqlite3')
-
-# step 2 - make the cursor
-cursor = connection.cursor()
-
-
-# step 3 -write a query
-# see queries.py file
-
-# step 4 - Execute the query on the cursor and fetch the results
-# pulling the results from the query
-results = cursor.execute(q.SELECT_ALL).fetchall()
+def execute_q(conn, query):
+    # step 2 - make the cursor
+    curs = conn.cursor()
+    # execute query
+    curs.execute(query)
+    # pull and return the results
+    return curs.fetchall()
 
 if __name__ == '__main__':
-    print(results[:5])
+    conn = connect_to_db()
+    #print(execute_q(conn, q.SELECT_ALL)[:5])
+    results = execute_q(conn, q.AVG_ITEM_WEIGHT_PER_CHARACTER)
+    df = pd.DataFrame(results)
+    df.columns = ['name','average_item_weight']
+    df.to_csv('rpg_db.csv', index = False)
